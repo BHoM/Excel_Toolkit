c2db05ed1e95556e87570383b1d1848979de482e
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:08 +0100

    Update Assembly Information

--- Excel_Engine/Properties/AssemblyInfo.cs
+++ Excel_Engine/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Excel_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Excel_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BuroHappold")]
 [assembly: AssemblyProduct("Excel_Engine")]
